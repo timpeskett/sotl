5d94b4661932cf86eb08232aa88161fea5af40e7
Tim Peskett <timpeskett@live.com>
2016-01-29 22:45:48 +0800

    Initial project commit

---
+++ proj/desktop/src/net/thirteen/sotl/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.thirteen.sotl.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.thirteen.sotl.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
