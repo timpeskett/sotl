522dc18749867f68a526975d344b20602868a46c
mercluke <mercluke@gmail.com>
2016-01-30 05:48:29 +0800

    can press enter on title screen

--- proj/core/src/net/thirteen/sotl/screens/TitleScreen.java
+++ proj/core/src/net/thirteen/sotl/screens/TitleScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 
 public class TitleScreen implements Screen {
 
@@ -41,7 +42,8 @@ public class TitleScreen implements Screen {
 	}
 
 	public void handleInput(float delta) {
-		if(Gdx.input.isTouched()) {
+		if(Gdx.input.isTouched() || 
+		   Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
 			game.setScreen(new LevelScreen(game));
 			dispose();
 		}
