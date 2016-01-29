f20874563450243fbb348fa280f0db4b20d62bef
Tim Peskett <timpeskett@live.com>
2016-01-30 07:17:59 +0800

    Added LevelMaker

---
+++ proj/core/src/net/thirteen/sotl/levels/LevelMaker.java
@@ -0,0 +1,62 @@
+package net.thirteen.sotl.levels;
+
+import net.thirteen.sotl.tiles.Tile;
+import net.thirteen.sotl.tiles.DoorTile;
+import net.thirteen.sotl.tiles.WallTile;
+import net.thirteen.sotl.tiles.GrassTile;
+import net.thirteen.sotl.World;
+import net.thirteen.sotl.utils.Tuple;
+
+import com.badlogic.gdx.math.Rectangle;
+
+import java.util.HashMap;
+
+public class LevelMaker {
+    private int dimX, dimY;
+    private Rectangle bounds;
+
+    /* dimX and dimY are the number of horizontal and vertical tiles
+     * respectively */
+    public LevelMaker(int dimX, int dimY, Rectangle bounds) {
+        this.dimX = dimX;
+        this.dimY = dimY;
+
+        this.bounds = bounds;
+    }
+
+    
+    public Level generate(HashMap<Tuple, DoorTile> entrances, int difficulty, World world) {
+        Tile [][] tileMap = new Tile[dimX][dimY];
+
+        /* Start simple. Fill map with grass */
+        for(int x = 0; x < dimX; x++) {
+            for(int y = 0; y < dimY; y++) {
+                tileMap[x][y] = new GrassTile();
+            }
+        }
+
+        for(int x = 0; x < dimX; x++) {
+            //we'll add a door
+            if(x == 8){
+                tileMap[x][dimY-1] = new DoorTile();
+            }
+            else{
+                tileMap[x][dimY-1] = new WallTile();
+            }
+
+            tileMap[x][0] = new WallTile();
+        }
+            
+        for(int y = 0; y < dimY; y++) {
+            tileMap[0][y] = new WallTile();
+            tileMap[dimX-1][y] = new WallTile();
+        }
+
+        return new Level(dimX, dimY, bounds, tileMap, world);
+
+    }
+
+    public Level load(/* Load from file */) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
