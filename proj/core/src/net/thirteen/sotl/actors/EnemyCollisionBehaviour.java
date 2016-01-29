430e787f568a954de71636b987da2f54caec5082
mercluke <mercluke@gmail.com>
2016-01-30 05:44:54 +0800

    changed collisions to classes, now subclasses differ purely by distance, yay for wasted memory

---
+++ proj/core/src/net/thirteen/sotl/actors/EnemyCollisionBehaviour.java
@@ -0,0 +1,63 @@
+package net.thirteen.sotl.actors;
+
+import net.thirteen.sotl.levels.Level;
+import net.thirteen.sotl.actors.Hero;
+import net.thirteen.sotl.tiles.Tile;
+import com.badlogic.gdx.math.Rectangle;
+import net.thirteen.sotl.actors.Direction;
+import net.thirteen.sotl.utils.Tuple;
+
+public class EnemyCollisionBehaviour {
+
+	protected int distance = 0;
+
+	public boolean collide(Level lev, Rectangle rect, Direction direction){
+		
+		
+		//convert from x/y co-ords to tile co-ords
+		Tuple tileTuple = lev.worldToTile(rect.x, rect.y);
+		int tileX = tileTuple.first();
+		int tileY = tileTuple.last();
+		Hero hero = lev.getHero();
+
+        if(rect.overlaps(hero.getRekt())){
+        	return true;
+        }
+
+		//mediumRange can see 2 ahead
+		for(int i = 0; i < 2; i++){
+
+			Tile checkTile;
+
+			switch (direction){
+				case UP:
+					tileY++;
+					break;
+				case DOWN:
+					tileY--;
+					break;
+				case RIGHT:
+					tileX++;
+					break;
+				case LEFT:
+					tileX--;
+					break;
+			}
+
+
+			checkTile = lev.getTile((int)tileX, (int)tileY);
+
+			//Don't check further tiles if looking at wall/etc
+			if(!checkTile.isTileTransparent()){
+				return false;
+			}
+
+			if(lev.isActorAtTile(lev.getHero(), (int)tileX, (int)tileY)){
+				return true;
+			}
+
+		}
+
+		return false;
+	}
+}
--- proj/core/src/net/thirteen/sotl/actors/MediumRangeCollision.java
+++ proj/core/src/net/thirteen/sotl/actors/MediumRangeCollision.java
@@ -1,18 +1,12 @@
 package net.thirteen.sotl.actors;
 
-import net.thirteen.sotl.levels.Level;
-import net.thirteen.sotl.actors.Hero;
-import net.thirteen.sotl.tiles.Tile;
-import com.badlogic.gdx.math.Rectangle;
-import net.thirteen.sotl.actors.Direction;
-import net.thirteen.sotl.utils.Tuple;
-
-public class MediumRangeCollision implements IEnemyCollisionBehaviour {
+public class MediumRangeCollision extends EnemyCollisionBehaviour {
 	
 	private static MediumRangeCollision instance;
 
 	private MediumRangeCollision(){
 		//singleton
+		distance = 2;
 	}
 
 	public static MediumRangeCollision getInstance(){
@@ -21,50 +15,6 @@ public class MediumRangeCollision implements IEnemyCollisionBehaviour {
 		}
 
 		return instance;
-	}
-
-	public boolean collide(Level lev, Rectangle rect, Direction direction){
-		
-		//convert from x/y co-ords to tile co-ords
-		Tuple tileTuple = lev.worldToTile(rect.x, rect.y);
-		int tileX = tileTuple.first();
-		int tileY = tileTuple.last();
-
-		//mediumRange can see 2 ahead
-		for(int i = 0; i < 2; i++){
-
-			Tile checkTile;
-
-			switch (direction){
-				case UP:
-					tileY++;
-					break;
-				case DOWN:
-					tileY--;
-					break;
-				case RIGHT:
-					tileX++;
-					break;
-				case LEFT:
-					tileX--;
-					break;
-			}
-
-
-			checkTile = lev.getTile((int)tileX, (int)tileY);
-
-			//Don't check further tiles if looking at wall/etc
-			if(!checkTile.isTileTransparent()){
-				return false;
-			}
-
-			// if(lev.isActorAtTile(lev.getHero(), (int)tileX, (int)tileY)){
-			// 	return true;
-			// }
-
-		}
-
-		return false;
 	}
 
 }
