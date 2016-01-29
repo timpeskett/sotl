980a08ba706276a94e8950ebe458151533515e40
Tim Peskett <timpeskett@live.com>
2016-01-29 22:59:05 +0800

    Configured gradle for testing. Created test template file

---
+++ proj/desktop/test/net/thirteen/sotl/TestTemplate.java
@@ -0,0 +1,14 @@
+package net.thirteen.sotl;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestTemplate {
+    @Test
+    public void someTest() {
+        
+        /* Do testing here */
+
+        assertEquals(153, 100 + 53);
+    }
+}
