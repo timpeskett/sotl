a7dd4bd751d4aec6da0e2cf051e1080bb6b4e31e
mercluke <mercluke@gmail.com>
2016-01-30 03:37:13 +0800

    Medium range collision maybe works?

--- proj/core/src/net/thirteen/sotl/actors/MediumRangeCollision.java
+++ proj/core/src/net/thirteen/sotl/actors/MediumRangeCollision.java
@@ -2,6 +2,7 @@ package net.thirteen.sotl.actors;
 
 import net.thirteen.sotl.levels.Level;
 import net.thirteen.sotl.actors.Hero;
+import net.thirteen.sotl.tiles.Tile;
 import com.badlogic.gdx.math.Rectangle;
 import net.thirteen.sotl.actors.Direction;
 
@@ -23,6 +24,39 @@ public class MediumRangeCollision implements IEnemyCollisionBehaviour {
 
 	public boolean collide(Level lev, Rectangle rect, Direction direction){
 		
+		float tileX = rect.x / lev.getTileWidth();
+		float tileY = rect.y / lev.getTileHeight();
+		Tile checkTile;
+
+		//mediumRange can see 2 ahead
+		for(int i = 0; i < 2; i++){
+			switch (direction){
+				case UP:
+					tileY++;
+					break;
+				case DOWN:
+					tileY--;
+					break;
+				case RIGHT:
+					tileX++;
+					break;
+				case LEFT:
+					tileX--;
+					break;
+			}
+
+			checkTile = lev.getTile((int)tileX, (int)tileY);
+
+			if(!checkTile.isTileTransparent()){
+				return false;
+			}
+
+			// if(lev.isActorAtTile(lev.getHero(), (int)tileX, (int)tileY)){
+			// 	return true;
+			// }
+
+		}
+
 		//have some code that checks whether tiles in 
 		//direction x are transparent/the hero
 
